84acb5c6181de1452fd18ba1852a8a632b4fbba4
seokminlee24 <lsm67286564@gmail.com>
2024-11-05 17:08:01 +0900

    component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App2 from "./assets/practice/App2.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App2 />
+  </StrictMode>,
+);
