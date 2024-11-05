b407201ceef70659aba70a4797a60c901a78ae8c
seokminlee24 <lsm67286564@gmail.com>
2024-11-05 17:07:35 +0900

    react 맛보기

---
+++ src/apps/MyApp1.jsx
@@ -0,0 +1,16 @@
+function MyApp1() {
+  return (
+    <div>
+      첫번째 app
+      <p>Lorem ipsum dolor.</p>
+      <ul>
+        <li>Lorem.</li>
+        <li>Sint?</li>
+        <li>Provident!</li>
+      </ul>
+      <h3>Lorem.</h3>
+    </div>
+  );
+}
+
+export default MyApp1;
