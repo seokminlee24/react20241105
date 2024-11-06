c46923fce27b146dfa53c59355070bc45b022427
seokminlee24 <lsm67286564@gmail.com>
2024-11-06 16:43:25 +0900

    chakra ui 맛보기

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp24 from "./apps/MyApp23.jsx";
+import MyApp25 from "./apps/MyApp25.jsx";
+import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp24 />
+      <MyApp25 />
     </Provider>
   </StrictMode>,
 );
