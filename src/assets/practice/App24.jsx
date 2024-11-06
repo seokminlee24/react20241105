2c66cafdea6e15d519a1c5f6469b1d66e8f13e5f
seokminlee24 <lsm67286564@gmail.com>
2024-11-06 16:43:40 +0900

    style prop

--- src/assets/practice/App24.jsx
+++ src/assets/practice/App24.jsx
@@ -3,13 +3,24 @@ import React from "react";
 function App24(props) {
   return (
     <div>
-      <MyButton />
+      <MyButton color="black">클릭하세요</MyButton>
+      <hr />
+      <MyButton color="yellow">다시 클릭하세요.</MyButton>
     </div>
   );
 }
 
-function MyButton(color) {
-  return <div></div>;
+function MyButton({ color, children }) {
+  return (
+    <button
+      style={{
+        backgroundColor: color,
+        color: color === "black" ? "yellow" : "black",
+      }}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default App24;
