cac140fb1b7c5f01c1b5cf59b7c3bfceec231f60
seokminlee24 <lsm67286564@gmail.com>
2024-11-06 16:36:33 +0900

    chakra ui 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
+import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MyApp24 from "./apps/MyApp23.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp24 />
+    <Provider>
+      <MyApp24 />
+    </Provider>
   </StrictMode>,
 );
