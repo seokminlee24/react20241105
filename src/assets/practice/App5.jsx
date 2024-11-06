c671b292299fc502c43704d65fa0a1c81dad076b
seokminlee24 <lsm67286564@gmail.com>
2024-11-06 09:50:38 +0900

    fragment

---
+++ src/assets/practice/App5.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App5(props) {
+  return (
+    <>
+      <h1 className="error">lorem</h1>
+      <div>
+        <label htmlFor="check1">동의</label>
+        <input type="checkbox" id="check1" />
+      </div>
+      <hr />
+      <h3>hello</h3>
+    </>
+  );
+}
+export default App5;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App4 from "./assets/practice/App4.jsx";
+import App5 from "./assets/practice/App5.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App4 />
+    <App5 />
   </StrictMode>,
 );
