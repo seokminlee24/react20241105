a26e2558e548348c7dcc6205c9dac4b4758105af
seokminlee24 <lsm67286564@gmail.com>
2024-11-06 12:41:22 +0900

    export, import

---
+++ src/apps/MyApp14.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import zx, { a, b, f as fa } from "./MyApp13.jsx";
+import { d, e as ea, f as fb } from "./MyApp15.jsx";
+
+function MyApp14(props) {
+  return (
+    <div>
+      <h1>myapp 14</h1>
+      <h1>{a}</h1>
+      <h1>{b}</h1>
+      <h1>{zx}</h1>
+      <h1>{d}</h1>
+      <h1>{ea}</h1>
+      <h1>
+        {fa}, {fb}
+      </h1>
+    </div>
+  );
+}
+
+export default MyApp14;
