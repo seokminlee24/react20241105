e41f2bd607887b313651bc6fee4295626768fb0b
seokminlee24 <lsm67286564@gmail.com>
2024-11-07 09:24:14 +0900

    버튼

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp25 from "./apps/MyApp25.jsx";
 import { Provider } from "./components/ui/provider.jsx";
+import MyApp26 from "./apps/MyApp26.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp25 />
+      <MyApp26 />
     </Provider>
   </StrictMode>,
 );
