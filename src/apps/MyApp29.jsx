667771399b1aa2252d88f3c4b5ffc632c4514af3
seokminlee24 <lsm67286564@gmail.com>
2024-11-07 16:54:49 +0900

    pagination

---
+++ src/apps/MyApp29.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  PaginationItems,
+  PaginationNextTrigger,
+  PaginationPrevTrigger,
+  PaginationRoot,
+} from "../components/ui/pagination.jsx";
+import { HStack } from "@chakra-ui/react";
+
+function MyApp29(props) {
+  return (
+    <div>
+      <h5>pagination</h5>
+      <PaginationRoot
+        siblingCount={3}
+        count={1024}
+        pageSize={10}
+        variant={"outline"}
+      >
+        <HStack>
+          <PaginationPrevTrigger />
+          <PaginationItems />
+          <PaginationNextTrigger />
+        </HStack>
+      </PaginationRoot>
+      <PaginationRoot count={1024} pageSize={10}>
+        <HStack>
+          <PaginationPrevTrigger />
+          <PaginationItems />
+          <PaginationNextTrigger />
+        </HStack>
+      </PaginationRoot>
+    </div>
+  );
+}
+
+export default MyApp29;
