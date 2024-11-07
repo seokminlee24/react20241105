6070c4c90b6c04b220d328e3899447d7a55e0f8f
seokminlee24 <lsm67286564@gmail.com>
2024-11-07 16:53:55 +0900

    state

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp28 from "./apps/MyApp28.jsx";
+import { Toaster } from "./components/ui/toaster.jsx";
+import App39 from "./assets/practice/App39.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp28 />
+      <App39 />
+      <Toaster />
     </Provider>
   </StrictMode>,
 );
