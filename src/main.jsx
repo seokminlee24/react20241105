df74d06d73551a19e139c6e83b5fc03dcf5bb337
seokminlee24 <lsm67286564@gmail.com>
2024-11-08 15:05:14 +0900

    lifting state

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp43 from "./apps/MyApp43.jsx";
+import App48 from "./assets/practice/App48.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider>
-      <MyApp43 />
-      <Toaster />
-    </Provider>
-  </StrictMode>,
+  //<StrictMode>
+  <Provider>
+    <App48 />
+    <Toaster />
+  </Provider>,
+  //</StrictMode>,
 );
