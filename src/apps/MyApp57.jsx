37c9d52a548a645a2b788ff92636055c500d7537
seokminlee24 <lsm67286564@gmail.com>
2024-11-11 14:07:20 +0900

    import React from "react"; import { Button } from "../components/ui/button.jsx";
    function MyApp57(props) {
      return (
        <div>
          {/*javascript 사용*/}
          <Button onClick={() => {}}>get 요청</Button>
          <Button onClick={() => {}}>post 요청</Button>
          <hr />
          {/* html 요소 이용 (a, form) */}
          {/* get */}
          <a href="/hi">to hi</a>
          <hr />
          <form action="/hello" method="get">
            <button>전송</button>
          </form>
          <hr />
          {/*  post */}
          <form action="/greeting" method="post">
            <button>전송</button>
          </form>
        </div>
      );
    }
    
    export default MyApp57;

---
+++ src/apps/MyApp57.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function MyApp57(props) {
+  return (
+    <div>
+      {/*javascript 사용*/}
+      <Button onClick={() => {axios}}
+      <hr/>
+      {/*  get 방식*/}
+      <a href="/hi">to hi</a>
+      <hr />
+      <form action="/hello" method="get">
+        <button>전송</button>
+      </form>
+      <hr />
+      <form action="/greeting" method="post">
+        <button>전송</button>
+      </form>
+    </div>
+  );
+}
+
+export default MyApp57;
