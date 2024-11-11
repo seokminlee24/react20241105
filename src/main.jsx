d15f1aabc093cad2445112380570ac23657caaa8
seokminlee24 <lsm67286564@gmail.com>
2024-11-11 09:59:42 +0900

    component lifecycle

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp51 from "./apps/MyApp51.jsx";
+import MyApp52 from "./apps/MyApp52.jsx";
 
 createRoot(document.getElementById("root")).render(
   //<StrictMode>
   <Provider>
-    <MyApp51 />
+    <MyApp52 />
     <Toaster />
   </Provider>,
   //</StrictMode>,
