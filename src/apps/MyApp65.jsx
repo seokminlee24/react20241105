6de120a32742b8cb986978c1e1277eabc97c4a1b
seokminlee24 <lsm67286564@gmail.com>
2024-11-12 14:54:07 +0900

    response

--- src/apps/MyApp65.jsx
+++ src/apps/MyApp65.jsx
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../components/ui/button.jsx";
 import axios from "axios";
+import { Table } from "@chakra-ui/react";
 
+function MyBookTableView({ books }) {
+  if (!books || books.length == 0) {
+    return <div>책 목록을 조회해 주세요.</div>;
+  }
+  return (
+    <>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>TITLE</Table.ColumnHeader>
+            <Table.ColumnHeader>CONTENT</Table.ColumnHeader>
+            <Table.ColumnHeader>AUTHOR</Table.ColumnHeader>
+            <Table.ColumnHeader>PRICE</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {books.map((book) => (
+            <Table.Row>
+              <Table.Cell>{book.title}</Table.Cell>
+              <Table.Cell>{book.content}</Table.Cell>
+              <Table.Cell>{book.author}</Table.Cell>
+              <Table.Cell>{book.price}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
+  );
+}
 function MyApp65(props) {
+  const [books, setBooks] = useState([]);
+
   return (
     <div>
+      <MyBookTableView books={books} />
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main5/sub10").then((res) => setBooks(res.data));
+        }}
+      >
+        btn11
+      </Button>
+      <Button
+        onClick={() => {
+          axios
+            .get("/api/main5/sub10")
+            .then((res) => res.data)
+            .then((data) => {
+              console.log(data[0].title);
+              console.log(data[0].author);
+              console.log(data[0].content);
+              console.log(data[0].price);
+
+              console.log(data[1].title);
+              console.log(data[1].author);
+              console.log(data[1].content);
+              console.log(data[1].price);
+
+              console.log(data[2].title);
+              console.log(data[2].author);
+              console.log(data[2].content);
+              console.log(data[2].price);
+            });
+        }}
+      >
+        btn10
+      </Button>
       <Button
         onClick={() => {
           axios
@@ -23,7 +89,6 @@ function MyApp65(props) {
       >
         btn9
       </Button>
-      <hr />
       <Button
         onClick={() => {
           axios
@@ -51,7 +116,6 @@ function MyApp65(props) {
       >
         btn7
       </Button>
-      <hr />
       <Button
         onClick={() => {
           axios
@@ -69,7 +133,6 @@ function MyApp65(props) {
       >
         btn6
       </Button>
-      <hr />
       <Button
         onClick={() => {
           axios
@@ -85,7 +148,6 @@ function MyApp65(props) {
       >
         btn5
       </Button>
-      <hr />
       <Button
         onClick={() => {
           axios
@@ -100,18 +162,16 @@ function MyApp65(props) {
       >
         btn4
       </Button>
-      <hr />
       <Button
         onClick={() => {
           axios
             .get("/api/main5/sub3")
-            .then((response) => response.data)
-            .then((date) => console.log(date));
+            .then((r) => r.data)
+            .then((d) => console.log(d));
         }}
       >
         btn3
       </Button>
-      <hr />
       <Button
         onClick={() => {
           axios
