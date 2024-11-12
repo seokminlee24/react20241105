b4b07a17e00aebffd5691a2a8935e045fe993168
seokminlee24 <lsm67286564@gmail.com>
2024-11-12 11:24:48 +0900

    path variable

---
+++ src/apps/MyApp64.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "../components/ui/button.jsx";
+import axios from "axios";
+
+function MyApp64(props) {
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          axios.get("/api/main4/sub2/777");
+        }}
+      >
+        경로 데이터 number
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main4/sub2/9000");
+        }}
+      >
+        경로 데이터 number
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main4/sub1/kim");
+        }}
+      >
+        경로 데이터(path valuable)
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main4/sub1/son");
+        }}
+      >
+        경로 데이터(path valuable)
+      </Button>
+    </div>
+  );
+}
+
+export default MyApp64;
