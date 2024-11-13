e88283c1ca18f4639d07e3ba4df2e4e6fd5b95f2
seokminlee24 <lsm67286564@gmail.com>
2024-11-13 10:52:08 +0900

    파일 전송 : postForm

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp68 from "./apps/MyApp68.jsx";
+import MyApp69 from "./apps/MyApp69.jsx";
 
 createRoot(document.getElementById("root")).render(
   //<StrictMode>
   <Provider>
-    <MyApp68 />
+    <MyApp69 />
     <Toaster />
   </Provider>,
   //</StrictMode>,
