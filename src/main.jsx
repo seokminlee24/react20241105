fed80fa3707463bea89d080fe917362eafdec6ce
seokminlee24 <lsm67286564@gmail.com>
2024-11-13 11:27:27 +0900

    파일 업로드

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp69 from "./apps/MyApp69.jsx";
+import App69 from "./assets/practice/App69.jsx";
 
 createRoot(document.getElementById("root")).render(
   //<StrictMode>
   <Provider>
-    <MyApp69 />
+    <App69 />
     <Toaster />
   </Provider>,
   //</StrictMode>,
