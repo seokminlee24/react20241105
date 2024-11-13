565e497a53f0933164dc9046e84d4a0acca958c8
seokminlee24 <lsm67286564@gmail.com>
2024-11-13 12:42:48 +0900

    nested router

--- src/apps/MyApp71.jsx
+++ src/apps/MyApp71.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom"; // /page3 으로 이동했을 때 어떤 컴포넌트 출력
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"; // /page3 으로 이동했을 때 어떤 컴포넌트 출력
 // /page3 으로 이동했을 때 어떤 컴포넌트 출력
 // /page4 으로 이동했을 때 어떤 컴포넌트 출력
 // /page1/sub1 으로 이동했을 때 어떤 컴포넌트 출력
@@ -8,28 +8,47 @@ const router = createBrowserRouter([
   { path: "/", element: <div>여기는 홈페이지</div> },
   {
     path: "/path1",
-    element: <div>여기는 1번 경로</div>,
+    element: (
+      <div>
+        여기는 1번 경로
+        <hr />
+        <Outlet />
+      </div>
+    ),
+    children: [
+      {
+        index: true,
+        /*path: ""*/ element: <div>여기는 경로1의 홈페이지</div>,
+      },
+      {
+        path: "sub1",
+        element: <div>여기는 경로1/서브1</div>,
+      },
+      {
+        path: "sub2",
+        element: <div>여기는 경로1/서브2</div>,
+      },
+    ],
   },
-  {
-    path: "/path1/sub1",
-    element: <div>여기는 1-1번 경로</div>,
-  },
-  {
-    path: "/path1/sub2",
-    element: <div>여기는 1-2번 경로</div>,
-  },
-  {
-    path: "/path2",
-    element: <div>여기는 2번 경로</div>,
-  },
+  //{ path: "/path1/sub1", element: <div>여기는 1-1번 경로</div> },
+  //{ path: "/path1/sub2", element: <div>여기는 1-2번 경로</div> },
+  { path: "/path2", element: <div>여기는 2번 경로</div> },
   {
     path: "/path3",
-    element: <div>여기는 3번 경로</div>,
+    element: (
+      <div>
+        여기는 3번 경로
+        <hr />
+        <Outlet />
+      </div>
+    ),
+    children: [
+      { index: true, element: <div>여기는 경로3의 홈페이지</div> },
+      { path: "sub1", element: <div>여기는 경로3/서브1</div> },
+      { path: "sub2", element: <div>여기는 경로3/서브2</div> },
+    ],
   },
-  {
-    path: "/path4",
-    element: <div>여기는 4번 경로</div>,
-  },
+  { path: "/path4", element: <div>여기는 4번 경로</div> },
 ]);
 
 function MyApp71(props) {
