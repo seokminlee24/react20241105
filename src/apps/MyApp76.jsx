ae5c57dc5d2bc36cb9241dd44b9c62588010af9f
seokminlee24 <lsm67286564@gmail.com>
2024-11-13 15:34:44 +0900

    searchParams

---
+++ src/apps/MyApp76.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+import { Button } from "../components/ui/button.jsx";
+
+function MyHome() {
+  const navigate = useNavigate();
+  const [text, setText] = useState("");
+  const [params, setParams] = useSearchParams();
+
+  console.log(params.toString());
+
+  return (
+    <Box>
+      <Button
+        onClick={() => {
+          const p = new URLSearchParams();
+          p.append("page", 1);
+          setParams(p);
+        }}
+      >
+        1
+      </Button>
+      <Button
+        onClick={() => {
+          const p = new URLSearchParams();
+          p.append("page", 2);
+          setParams(p);
+        }}
+      >
+        2
+      </Button>
+      <Button
+        onClick={() => {
+          const p = new URLSearchParams();
+          p.append("page", 3);
+          setParams(p);
+        }}
+      >
+        3
+      </Button>
+    </Box>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MyHome />,
+  },
+]);
+
+function MyApp76(props) {
+  return <RouterProvider router={router} />;
+}
+
+export default MyApp76;
